af74cb6c3f47725be4d44a882f088a823c5318f0
UltraV <1216564567@qq.com>
2024-05-08 10:21:56 +0800

    添加测试项

--- test_coze.js
+++ test_coze.js
@@ -2,16 +2,16 @@ const response = await fetch('http://localhost:3000/v1/chat/completions', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer pat_kTOtCOx1H1wGaafTFHh2nQ3OPzUY5ZHLgLUSgvE4ihQalK1lcd4zATsrWPvL8rjp',
+    'Authorization': 'Bearer pat_BYAN3UTjLd6075hEjYiN3e2tGZxcelbETnITxESMIWJb0xQg36uW7qSnhnFEcWiA',
   },
   body: JSON.stringify({
-    model: 7363122563914432520,
+    model: 7363127235433742344,
     messages: [
       { role: 'system', content: 'You are a helpful assistant.' },
-      { role: 'user', content: 'Hello, how are you?' },
+      { role: 'user', content: '什么是索飞安全？' },
     ],
   }),
 });
 
 const data = await response.json();
-console.log(data);
+console.log(data.choices[0].message.content);
